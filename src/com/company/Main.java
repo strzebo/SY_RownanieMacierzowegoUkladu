1f021d8a5c76430b345597a3e93faed8219060e3
strzebo <strzebo@o2.pl>
2018-01-02 22:46:38 +0100

    założenie klas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,9 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello World");
     }
 }
