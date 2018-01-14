e6ae611b911042d0af096ed8406d34b60f480e91
strzebo <strzebo@o2.pl>
2018-01-14 18:29:11 +0100

    Obliczenia c.d.

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,34 @@
 package com;
 
+import java.io.Console;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        System.out.printf("Hello World");
+        float[][] g = new float[3][3];
+        float[] i = new float[3];
+        i[0] = 10;
+        i[1] = 0;
+        i[2] = 0;
+        g[0][0] = 1;
+        g[0][1] = -1;
+        g[0][2] = 0;
+        g[1][0] = -1;
+        g[1][1] = 2.125f;
+        g[1][2] = -0.625f;
+        g[2][0] = 0;
+        g[2][1] = -0.625f;
+        g[2][2] = 3.125f;
+
+        Obliczenia obliczenia = new Obliczenia(i, g);
+        Obliczenia.Wynik wynik = obliczenia.Oblicz();
+        for(int v = 0; v < wynik.V.length; v++)
+        {
+            int zmienna = v + 1;
+            System.out.printf("V" + zmienna + " : " + wynik.V[v]);
+        }
+
+        System.out.println();
     }
 }
--- src/com/Obliczenia.java
+++ src/com/Obliczenia.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.io.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Obliczenia
@@ -13,6 +15,13 @@ public class Obliczenia
         this.OryginalneG = oryginalneG;
         this.OryginalneI = oryginalneI;
         this.Wielkosc = Math.sqrt(oryginalneG.length);
+    }
+
+    public Wynik Oblicz() throws Exception
+    {
+        int iloscPrzeksztalcen = IlePrzeksztalcen(false);
+
+        return ObliczWynik(iloscPrzeksztalcen);
     }
 
 
@@ -29,32 +38,133 @@ public class Obliczenia
         }
         return wynik;
     }
-}
 
-
-class Wynik
-{
-    public List<Macierze> WszystkieMacierze;
-    public float[] V;
-
-    public Wynik(List<Macierze> macierze, float[] v)
+    private Wynik ObliczWynik(int iloscPrzeksztalcen) throws Exception
     {
-        this.WszystkieMacierze = macierze;
-        this.V = v;
+        ArrayList<Macierze> wszystkieMacierze = new ArrayList<Macierze>();
+        wszystkieMacierze.add(new Macierze(KopiujG(this.OryginalneG), this.OryginalneI, 0));
+
+        for (int przeksztalcenie = iloscPrzeksztalcen; przeksztalcenie < this.Wielkosc; przeksztalcenie++)
+        {
+            Macierze macierzPoprzednia = wszystkieMacierze.get(przeksztalcenie - 1);
+            float[][] macierz = KopiujG(macierzPoprzednia.G);
+            float[] noweI = KopiujI(macierzPoprzednia.I);
+
+            wszystkieMacierze.add(new Macierze(macierz, noweI, przeksztalcenie));
+            for (int i = przeksztalcenie; i < this.Wielkosc; i++)
+            {
+                for (int j = przeksztalcenie - 1; j < this.Wielkosc; j++)
+                {
+                    macierz[i][j] = ObliczPrzeksztalcenie(i, j, przeksztalcenie, macierzPoprzednia.G);
+                    System.out.printf("Modyfikacja " + przeksztalcenie + ":" + macierz[i][j]);
+                }
+                noweI[i] = ObliczNoweI(i, przeksztalcenie, macierzPoprzednia.I, macierzPoprzednia.G);
+                System.out.printf("I " + przeksztalcenie + ":" + noweI[i]);
+            }
+        }
+
+        return new Wynik(wszystkieMacierze, ObliczV(wszystkieMacierze.get(wszystkieMacierze.size() - 1) ));
     }
+
+
+    private float[] ObliczV(Macierze ostatniaMacierz) throws Exception
+    {
+        if (ostatniaMacierz == null)
+            throw new Exception("Nie obliczono wszystkich macierzy !");
+
+        float[] wynik = new float[ostatniaMacierz.Id + 1];
+        for (int i = ostatniaMacierz.Id; i >= 0; i--)
+        {
+            if (i == ostatniaMacierz.Id)
+                wynik[i] = WyliczWynikV(ostatniaMacierz, i, wynik, true);
+            else
+                wynik[i] = WyliczWynikV(ostatniaMacierz, i, wynik, false);
+        }
+        return wynik;
+    }
+
+    private float WyliczWynikV(Macierze macierz, int id, float[] poprzednieV, boolean czyOstatnie )
+    {
+        if (czyOstatnie)
+            return macierz.I[id] / macierz.G[id][id];
+
+        float suma = 0f;
+
+        for(int i = id; i < macierz.Id; i++)
+        {
+            suma += macierz.G[id][i + 1] * poprzednieV[i + 1];
+        }
+        return (macierz.I[id] - suma) / macierz.G[id][id];
+    }
+
+    private float ObliczNoweI(int i, int przeksztalcenie, float[] poprzednieI, float[][] poprzednia)
+    {
+        float mianownik = poprzednia[przeksztalcenie - 1][przeksztalcenie - 1];
+        if (mianownik == 0)
+            mianownik = 1;
+
+        return poprzednieI[przeksztalcenie] - ( poprzednieI[przeksztalcenie - 1] * ((poprzednia[i][przeksztalcenie - 1]) / mianownik));
+    }
+
+    private float ObliczPrzeksztalcenie(int i, int j, int przeksztalcenie, float[][] poprzednia) throws Exception
+    {
+        //int pomniejszoneI = i - 1 >= 0 ? i - 1 : 0;
+        if (i > this.OryginalneG.length)
+            throw new Exception("Zbyt dużo przekształceń");
+
+        float mianownik = poprzednia[przeksztalcenie - 1][przeksztalcenie - 1];
+        if (mianownik == 0)
+            mianownik = 1;
+
+        return poprzednia[i][j] - ((poprzednia[przeksztalcenie - 1][j]) * ((poprzednia[i][przeksztalcenie - 1]) / mianownik));
+    }
+
+    private float[] KopiujI(float[] poprzednieI)
+    {
+        float[] newI = new float[(int)this.Wielkosc];
+        for(int i = 0; i < this.OryginalneI.length ; i++)
+        {
+            newI[i] = poprzednieI[i];
+        }
+        return newI;
+    }
+    private float[][] KopiujG(float[][] macierzKopiowana)
+{
+    float[][] kopia = new float[(int)this.Wielkosc][(int)this.Wielkosc];
+    for(int i = 0; i < this.Wielkosc; i++)
+    {
+        for(int j = 0; j < this.Wielkosc; j++)
+        {
+            kopia[i][j] = macierzKopiowana[i][j];
+        }
+    }
+    return kopia;
 }
 
-class Macierze
-{
-    public float[][] G;
-    public float[] I;
-    public int Id;
-
-    public Macierze(float[][] g, float[] i, int id)
+    class Wynik
     {
-        this.G = g;
-        this.I = i;
-        this.Id = id;
+        public List<Macierze> WszystkieMacierze;
+        public float[] V;
+
+        public Wynik(List<Macierze> macierze, float[] v)
+        {
+            this.WszystkieMacierze = macierze;
+            this.V = v;
+        }
+    }
+
+    class Macierze
+    {
+        public float[][] G;
+        public float[] I;
+        public int Id;
+
+        public Macierze(float[][] g, float[] i, int id)
+        {
+            this.G = g;
+            this.I = i;
+            this.Id = id;
+        }
     }
 
     public static void rysujMacierz(float[][] macierz) {
